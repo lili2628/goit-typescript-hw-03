ee2552945fc00fa63b60b263b405a57464075f58
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-11-05 17:06:03 +0200

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+
+class Key {
+    private signature: number;
+    
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+
+
+  class Person{
+    constructor(private key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+
+  abstract class House {
+    public door: boolean = false; 
+    public key: Key;
+    private tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+      }
+    }
+
+    abstract openDoor(key: Key): void; 
+  }
+
+
+  class MyHouse extends House {
+
+    openDoor(key: Key) {
+        if (key.getSignature() ==== key.getSignature()) {
+            this.door = true;
+        }
+    }
+  }
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
